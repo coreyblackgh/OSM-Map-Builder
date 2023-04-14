8361dbcff3741b5d6db33e5d6c3a7082a7bf989c
Artem Hladkyi <Artem.Hladkyi@infopulse.com>
2023-04-14 10:14:48 +0300

    fix(config): fixed Portugal

--- run - Portugal_id_103.bat
+++ run - Portugal_id_103.bat
@@ -1,9 +1,9 @@
 rem This block setup specific map settings (MapId should be unique too!)
 rem Select desired map from: https://download.geofabrik.de and fill the fields below:
 set Region=europe
-set MapFileName=ukraine-latest.osm.pbf
-set MapName=OSM MapTourist Ukraine
-set MapDescription=MapTourist Ukraine Updated:
+set MapFileName=portugal-latest.osm.pbf
+set MapName=OSM MapTourist Portugal
+set MapDescription=MapTourist Portugal Updated:
 set MapId=103
 
 rem info MkgMap
