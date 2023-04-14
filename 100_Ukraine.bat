80346e9584941d53b3d54fce029a43a44c884d2c
Artem Hladkyi <Artem.Hladkyi@infopulse.com>
2023-04-14 11:43:45 +0300

    feat(sln): Refactored and added new styles

--- 100_Ukraine.bat
+++ 100_Ukraine.bat
@@ -6,15 +6,21 @@ set MapName=OSM MapTourist Ukraine
 set MapDescription=MapTourist Ukraine Updated:
 set MapId=100
 
-rem info MkgMap
-rem https://www.mkgmap.org.uk/doc/index.html
-rem The rest parameters are technical!
+rem Map style & Typ.
+set Typ=maptourist.typ
+set Style=Style_MapTourist
+
+rem Dirs.
 set MapsStoreFolder=.\MapsStoreFolder
+set TempOutput=.\TempOutput
+
+rem MkgMap & Splitter.
+rem https://www.mkgmap.org.uk/doc/index.html
 set SplitterFolder=.\splitter-r653
 set MkgMapFolder=.\mkgmap-r4907
 
+rem Logic.
 @echo Clean temp folders
-set TempOutput=.\TempOutput
 rmdir /Q /S %TempOutput%
 mkdir %TempOutput%
 
@@ -41,8 +47,8 @@ java -Xmx8G -jar splitter.jar --description="%MapDescription% %date:~-4,4%-%date
 cd..
 
 cd %MkgMapFolder%
-java -Xmx8G -jar mkgmap.jar --style-file=..\Configs\CurrentConfigs --check-styles
-java -Xmx8G -jar mkgmap.jar --series-name="%MapName%" --family-name="%MapName%" --family-id=%MapId% -c ..\Configs\config.cfg ..\Configs\maptourist.typ
+java -Xmx8G -jar mkgmap.jar --style-file=..\Configs\%Style% --check-styles
+java -Xmx8G -jar mkgmap.jar --series-name="%MapName%" --family-name="%MapName%" --family-id=%MapId% -c ..\Configs\config.cfg ..\Configs\%Typ%
 cd..
 
 cd %SplitterFolder%
