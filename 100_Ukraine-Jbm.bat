0f854983452d1043f119b7ef66bc1ffabc227669
Artem Hladkyi <Artem.Hladkyi@infopulse.com>
2023-04-15 19:17:13 +0300

    feat(sln): Added jbm and stranger configuration examples

---
+++ 100_Ukraine-Jbm.bat
@@ -0,0 +1,14 @@
+rem This block setup specific map settings (MapId should be unique too!)
+rem Select desired map from: https://download.geofabrik.de and fill the fields below:
+set Region=europe
+set MapFileName=ukraine-latest.osm.pbf
+set MapName=OSM Jbm Ukraine
+set MapDescription=Jbm Ukraine Updated:
+set MapId=100
+
+rem Map style & Typ.
+set Typ=jbm.typ
+set Style=Velo100Styles
+set Config=jbm.cfg
+
+GeneralMapBuilder.cmd
---
+++ 100_Ukraine-Stranger.bat
@@ -0,0 +1,14 @@
+rem This block setup specific map settings (MapId should be unique too!)
+rem Select desired map from: https://download.geofabrik.de and fill the fields below:
+set Region=europe
+set MapFileName=ukraine-latest.osm.pbf
+set MapName=OSM Stranger Ukraine
+set MapDescription=Stranger Ukraine Updated:
+set MapId=100
+
+rem Map style & Typ.
+set Typ=stranger.typ
+set Style=StrangerStyles
+set Config=stranger.cfg
+
+GeneralMapBuilder.cmd
